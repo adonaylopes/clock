a05b0ab695150ba041e9f5ad9f673b67f6af5939
adonaylopes <adonay023lopes@gmail.com>
2026-05-16 23:24:19 -0300

    Refactor time formatting in updateClock function for consistent display

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const clock = document.getElementById("clock");
 function updateClock() {
     const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
-    
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
     const Time = `${hours}:${minutes}:${seconds}`
     clock.innerText = Time;
     setInterval(updateClock, 1000);
 }
+updateClock();
